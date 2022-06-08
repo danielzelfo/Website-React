6b59c25e91ae3b5427b28d4497e325aeb8657dde
Daniel Zelfo <dzelfo@uci.edu>
2022-06-08 10:08:25 -0700

    Added projects with slides

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,54 +1,50 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useApp } from "../app/App";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Slide } from 'react-slideshow-image';
 import '../styles/project.scss';
 import 'react-slideshow-image/dist/styles.css'
+import data from "../data/portfolio.json";
 
 const Project = () => {
     const {
         setTitle
     } = useApp();
 
-    const {project_name} = useParams();
+    const { project_name } = useParams();
 
     useEffect(() => {
-        setTitle(project_name)
-    } , []);
-
-
-    const slideImages = [
-    {
-        url: 'https://imgur.com/sKV54PO.png',
-        caption: 'Space'
-    },
-    {
-        url: 'https://imgur.com/XMPcnz1.png',
-        caption: 'City'
-    },
-    {
-        url: 'https://imgur.com/fSgnUKW.png',
-        caption: 'Dogs'
-    }
-    ];
+        setTitle(data[project_name].title)
+    }, []);
 
     return (
         <main>
-			<div className="section" id="projectDemo">
-				
-                <Slide>
-                    {slideImages.map((slideImage, index)=> (
-                        <div className="each-slide" key={index}>
-                            <div style={{'backgroundImage': `url(${slideImage.url})`}}>
-                                <span>{slideImage.caption}</span>
-                            </div>
+            <div className="section" id="projectDemo">
+                {data[project_name].content.map((e) => (
+                    e.type === "slideshow" ?
+                        <Slide>
+                            {e.data.map((slideImage, index) => (
+                                <div className="each-slide" key={index}>
+                                    <div style={{ 'backgroundImage': `url(${slideImage.url})` }}>
+                                        <span>{slideImage.caption}</span>
+                                    </div>
+                                </div>
+                            ))}
+                        </Slide>
+                    :( e.type === "buttons" ?
+                        <div className="buttons">
+                            {
+                                e.data.map(btn => (
+                                    <a className="projbtn" href={btn.url} target="_blank" rel="noreferrer">{btn.text}</a>
+                                ))
+                            }
                         </div>
-                    ))}
-                </Slide>
-				<br /><a  className="btn black" href="https://github.com" target="_blank" rel="noreferrer"><span>Open GitHub</span></a><br />
-				<p className="description">Some description.</p>
-			</div>
-		</main>
+                    : e.type === "description" &&
+                        <p className="description">{e.data}</p>
+                    )
+                ))}
+            </div>
+        </main>
     );
 }
 
