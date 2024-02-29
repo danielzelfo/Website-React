89660c94c1613f3f1678aed5d61e7f59e0d348c5
Daniel Zelfo <daniel.zelfo@gmail.com>
2024-02-28 16:34:02 -0800

    allowTransparency

--- components/portfolio/PixelatedPaint.jsx
+++ components/portfolio/PixelatedPaint.jsx
@@ -1,6 +1,6 @@
 const PixelatedPaint = () => {
   return (
-    <iframe height="300" style={{width: "100%"}} scrolling="no" title="Pixelated Paint" src="https://codepen.io/dzelfo/embed/OggRKL?default-tab=js%2Cresult" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen="true">
+    <iframe height="300" style={{width: "100%"}} scrolling="no" title="Pixelated Paint" src="https://codepen.io/dzelfo/embed/OggRKL?default-tab=js%2Cresult" frameBorder="no" loading="lazy" allowFullScreen="true">
       See the Pen <a href="https://codepen.io/dzelfo/pen/OggRKL">
         Pixelated Paint</a> by Daniel (<a href="https://codepen.io/dzelfo">@dzelfo</a>)
       on <a href="https://codepen.io">CodePen</a>.
--- components/portfolio/ToDoList.jsx
+++ components/portfolio/ToDoList.jsx
@@ -1,6 +1,6 @@
 const TodoList = () => {
   return (
-    <iframe height="300" style={{width: "100%"}} scrolling="no" title="To-Do List" src="https://codepen.io/dzelfo/embed/pPezjy?default-tab=js%2Cresult" frameBorder="no" loading="lazy" allowtransparency="true" allowFullScreen="true">
+    <iframe height="300" style={{width: "100%"}} scrolling="no" title="To-Do List" src="https://codepen.io/dzelfo/embed/pPezjy?default-tab=js%2Cresult" frameBorder="no" loading="lazy" allowFullScreen="true">
       See the Pen <a href="https://codepen.io/dzelfo/pen/pPezjy">
         To-Do List</a> by Daniel (<a href="https://codepen.io/dzelfo">@dzelfo</a>)
       on <a href="https://codepen.io">CodePen</a>.
